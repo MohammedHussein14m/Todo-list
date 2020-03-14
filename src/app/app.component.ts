da455be52577eda23d833fca03a7851b569574f2
Mohammed Hussein <m.husein20120@gmail.com>
2020-03-14 13:34:11 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'ToDo-List';
+  title = "";
+  tasks = [];
+  isDoneValue =false ;
+  url = "http://localhost:3000/tasks/";
+
+  constructor(private http : HttpClient){
+  }
+
+  onClick() {
+    this.http.post(this.url,
+    { title : this.title , isDone : this.isDoneValue = false}).subscribe((data) => {
+      this.tasks.push(data);
+    })
+  }
+  remove(id) {
+    this.http.delete(`${this.url}${id}`).subscribe((data) => {
+    this.tasks = this.tasks.filter(task => {
+      return task.id !== id;
+  });
+ } )
+  }
+  isDone(id) {
+    this.isDoneValue = !this.isDoneValue;
+    this.http.put(`${this.url}${id}`,
+    {title : this.title, isDone : this.isDoneValue}).subscribe((data) => {
+      this.tasks.filter(t => {
+        if (t.id === id) {
+          t.isDone = !t.isDone;
+        }
+      });
+    });
+
+  }
 }
